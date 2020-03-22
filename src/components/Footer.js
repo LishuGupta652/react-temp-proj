16bf88197ec196bd5afb9fd00f692cead1aeae49
lishu gupta <lishugupta652@gmail.com>
2020-03-22 15:31:13 +0530

    header dev

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <div>
+          <h3>
+            <i class="fa fa-copyright" aria-hidden="true"></i> Lishu gupta 652.
+          </h3>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+import { TimelineMax } from "gsap";
+
+import { gsap } from "gsap";
+import { CSSPlugin } from "gsap/CSSPlugin";
+// Force CSSPlugin to not get dropped during build
+gsap.registerPlugin(CSSPlugin);
+
+const Header = () => {
+  const timeline = new TimelineMax();
+  timeline.fromTo(
+    ".header",
+    1,
+    { height: "0%" },
+    { height: "100%", ease: "Power2.easeInOut" }
+  );
+  return (
+    <header className="header">
+      <div className="wrapper iron-man">
+        <div className="wrapper center-content">
+          <div>
+            <h1>
+              <Typewriter
+                options={{
+                  strings: [
+                    "Designer",
+                    "Programmer",
+                    "Web Developer",
+                    "Android Developer"
+                  ],
+                  autoStart: true,
+                  loop: true,
+                  delay: 10,
+                  cursor: ".",
+                  start: 3000
+                }}
+              />
+            </h1>
+            <h4>and a little of everything.</h4>
+          </div>
+        </div>
+      </div>
+
+      <div className="logo">Lishu gupta</div>
+    </header>
+  );
+};
+
+export default Header;
