9aa93d88f5a26aecd8737dd338e5842f840a8ae5
lishu gupta <lishugupta652@gmail.com>
2020-03-23 00:46:53 +0530

    link

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import "./App.css";
 import Home from "./pages/Home";
 
 function App() {
-  const [cursorCords, setCursorCords] = React.useState({ x: 0, y: 0 });
-  useEffect(props => {
-    document.addEventListener("mousemove", e => {
-      // setCursorCords({ x: e.pageX, y: e.pageY });
-    });
-  });
+  // const [cursorCords, setCursorCords] = React.useState({ x: 0, y: 0 });
+  // useEffect(props => {
+  //   document.addEventListener("mousemove", e => {
+  //     // setCursorCords({ x: e.pageX, y: e.pageY });
+  //   });
+  // });
   return (
     <>
-      <div className="cursor" />
       <Home />
     </>
   );
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,12 +4,17 @@ const Skills = () => {
   return (
     <div className="skills">
       <div>
+        <h1>Skills</h1>
+      </div>
+      <div>
         <h1>Who am I</h1>
         <h3>Full Stack Web Developer, Android Developer and Programmer.</h3>
       </div>
       <div className="github">
         <h1>GitHub</h1>
-        <button>Connect</button>
+        <a href="https://www.github.com/lishugupta652" target="_">
+          <button>Connect</button>
+        </a>
       </div>
       <div className="resume">
         <h1>Resume</h1>
