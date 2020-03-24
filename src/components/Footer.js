f1590e7acc7323c97e6134e4726c1b9be33a4d04
lishu gupta <lishugupta652@gmail.com>
2020-03-24 17:58:27 +0530

    ncrm

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,19 @@ const Footer = () => {
     <div>
       <footer>
         <div>
-          <h3>
-            <i class="fa fa-copyright" aria-hidden="true"></i>{" "}
-            lishugupta652@gmail.com.
-          </h3>
+          <a
+            href="mailto:someone@example.com?Subject=Hello%20again"
+            target="_top"
+          >
+            <h3>
+              <i class="fa fa-copyright" aria-hidden="true"></i>{" "}
+              lishugupta652@gmail.com.
+            </h3>
+          </a>
+          <div>
+            Will catch you early.{" "}
+            <i class="fa fa-simplybuilt" aria-hidden="true"></i>
+          </div>
         </div>
       </footer>
     </div>
